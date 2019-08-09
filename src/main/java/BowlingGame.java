15541b475457f2e8f7be517ead943b2610904d56
chevlier <JanuszProgramowania123>
2019-08-09 10:33:38 +0200

    Initial commit

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,14 @@
+public class BowlingGame {
+
+    public void roll (int pins){}
+
+
+    public int score (){
+        int finalScore = 0;
+        return finalScore;
+    }
+
+
+
+
+}
---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,10 @@
+
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+
+        public void testGutterGame() throws Exception {
+            BowlingGame g = new BowlingGame();
+        }
+    }
